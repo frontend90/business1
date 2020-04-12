6cd546ab80e0de28c4e49a7faddc9c61b4ede2ba
frontend90 <leon44173@gmail.com>
2020-04-12 18:21:40 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+/*********************  плавный скролл    ***************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
+
 /************    header slick    **************/
 $('.header-slider').slick({
- arrows: false,
+  arrows: false,
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: true 
+  autoplay: true
 });
